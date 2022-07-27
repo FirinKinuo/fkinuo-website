ba602910c081a8ba38804da3a3731dfd2566c176
FirinKinuo <deals@fkinuo.ru>
2022-07-27 09:57:56 +0300

    Add: Current age calculation

--- src/components/information/modules/about/index.vue
+++ src/components/information/modules/about/index.vue
@@ -1,16 +1,29 @@
 <template>
   <article class="about">
-    <p class="text__black">Age: <span class="text__normal">19</span></p>
+    <p class="text__black">Age: <span class="text__normal">{{ Age }}</span></p>
     <p class="text__black">Langs: <span class="text__normal">Python, Golang, JavaScript</span></p>
 
-    <p class="text__black">Skills: <span class="text__normal">Backend Python & Go, Vue, CI/CD, docker, PostgreSQL, MongoDB, Redis, Linux</span></p>
+    <p class="text__black">Skills: <span class="text__normal">Backend Python & Go, Vue, CI/CD, docker, PostgreSQL, MongoDB, Redis, Linux</span>
+    </p>
 
   </article>
 </template>
 
 <script>
+import {mapGetters, mapMutations} from "vuex";
+
 export default {
-  name: "v-information-about"
+  name: "v-information-about",
+  computed: mapGetters({
+    Age: "getAge",
+  }),
+  methods: {
+    ...mapMutations(['calculateCurrentAge'])
+  },
+  beforeMount() {
+    this.calculateCurrentAge()
+  }
+
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import Information from "./modules/information"
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: {}
+    modules: {
+        Information
+    }
 });
