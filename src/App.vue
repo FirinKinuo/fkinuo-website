0c4f35b2d46cd5fb9b1fb0ee14e5cf8eaf576e26
FirinKinuo <deals@fkinuo.ru>
2023-05-10 17:56:41 +0300

    Add: New design, location of objects, information, background particles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <body class="single-page">
-    <div class="single-page__background"></div>
-    <v-information/>
-    <v-accent-image/>
+  <div class="single-page__background"></div>
+  <v-accent-image/>
+  <v-information/>
+  <v-background-particles/>
   </body>
 </template>
 
@@ -10,20 +11,20 @@
 
 import VInformation from "@/components/information";
 import VAccentImage from "@/components/accent-image";
+import VBackgroundParticles from "@/components/background-particles"
 
 export default {
   name: 'App',
-  components: {VAccentImage, VInformation}
+  components: {VAccentImage, VInformation, VBackgroundParticles}
 }
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;800&display=swap');
 @import "src/assets/scss/variables";
 
 :root {
   font-size: 18px;
-
   transition: 200ms ease-in;
 }
 
@@ -31,12 +32,13 @@ export default {
   outline: none;
   border: 0;
   text-decoration: none;
-  color: white;
-  font-family: 'JetBrains Mono', monospace;
+  color: $primary;
+  font-family: 'M PLUS Rounded 1c', sans-serif;
   scrollbar-width: thin;
-  scrollbar-color: $yellow $dark;
+  scrollbar-color: $primary $dark;
   margin: 0;
   padding: 0;
+  white-space: pre-wrap
 }
 
 *::-webkit-scrollbar {
@@ -48,49 +50,52 @@ export default {
 }
 
 *::-webkit-scrollbar-thumb {
-  background-color: $yellow;
+  background-color: $primary;
 }
 
 html {
   background-color: $dark;
   width: 100%;
+  max-width: 95vw;
   box-sizing: border-box;
 }
 
 .single-page {
   height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  padding: 0 30px;
+  overflow: hidden;
+  margin: 0 10px;
 
   &__background {
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-image: $tmp-image;
     background-size: cover;
-    filter: blur(5px) brightness(55%) contrast(120%);
+    background: $bg-gradient;
     transition: 200ms ease-in;
-    transform: scale(1.01);
     position: fixed;
     width: 100%;
     height: 100vh;
     top: 0;
     left: 0;
-    z-index: -1;
+    z-index: -1000;
   }
 }
 
-@media screen and (max-height: 700px), (max-width: 800px){
+@media screen and (max-width: 800px) {
   :root {
-    font-size: 16px;
+    font-size: 15px;
+  }
+
+  .single-page {
+    align-items: flex-start;
   }
 }
 
-@media screen and (max-width: 600px) {
-  :root {
-    font-size: 14px;
+@media screen and (max-height: 700px) {
+  .single-page {
+    overflow: scroll;
   }
 }
+
 
 </style>
