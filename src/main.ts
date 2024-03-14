479ea46732e8974acfc93972d47f6078580a2e3c
FirinKinuo <me@fkinuo.ru>
2024-03-14 06:03:26 +0300

    Update: Moving from Vue to Vanilla Vite + Tailwind, new design with morphing gradient background

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import './index.css'
+import './morphing-gradient-background.css'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector<HTMLDivElement>('.morphing-gradient_interactive')!;
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() => {
+            move();
+        });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+});
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"M PLUS Rounded 1c"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
